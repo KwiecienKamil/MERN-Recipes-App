0e4a21829e81314c4c16ee4ac9686c6b050e87f1
Kamil Kwiecień <kamilkwiecien9@gmail.com>
2023-12-27 11:55:35 +0100

    x

--- server/src/index.js
+++ server/src/index.js
@@ -2,12 +2,15 @@ import express from 'express'
 import cors from "cors"
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import { userRouter } from './routes/users.js';
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/auth", userRouter)
 
 const password = process.env.VITE_PASSWORD
 

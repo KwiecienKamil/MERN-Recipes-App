09f313f69dd7b18c6e409c77b86031766d151cb9
Kamil Kwiecień <kamilkwiecien9@gmail.com>
2023-12-27 14:11:27 +0100

    Register/Login

--- server/src/routes/users.js
+++ server/src/routes/users.js
@@ -9,8 +9,34 @@ router.post("/register", async (req,res) => {
     const {username, password} = req.body;
     const user = await UserModel.findOne({username}) // username:username => jeśli value i key są takie same to mozna napisac jedno
 
-    res.json(user)
+    if(user) {
+        return res.json({message: "User already exists"})
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    const newUser = new UserModel({username, password: hashedPassword})
+    await newUser.save()
+    res.json({message: "User Registered Successfully"})
 })
 
+router.post("/login", async (req,res) => {
+    const {username, password} = req.body;
+    const user = await UserModel.findOne({username})
+
+    if(!user) {
+        return res.json({message: "User Doesn't Exist"})
+    }
+
+
+    const isPasswordValid = await bcrypt.compare(password,user.password);
+
+    if(!isPasswordValid) {
+        return res.json({message: "Username or Password is Incorrect"})
+    }
+
+    const token = jwt.sign({id: user._id}, "secret");
+    res.json({token, userID:user._id})
+})
 
 export {router as userRouter};
